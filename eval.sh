647d08629bcc0f1644d50d71b6197eb34b187434
Gabriela <gmontoya@cs.aau.dk>
2018-03-26 10:28:28 +0200

    minor changes, improving eval.sh script

--- eval.sh
+++ eval.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
+timeoutMin=$4
+maxNumMappings=$5
+threshold=$6
+endpoint=$7
+
+e=""
+t=""
+m=""
+
+if [ -n "${endpoint}" ]; then
+  e="--endpointUrl ${endpoint}"
+fi
+
+if [ -n "${threshold}" ]; then
+  t="--threshold ${threshold}"
+fi
+
+if [ -n "${maxNumMappings}" ]; then
+  m="--maxNumberOfMappings ${maxNumMappings}"
+fi
+
 for query in $3/*.rq
 do
 	echo $query
 	echo "$1 -c $2 $query"
-	results=$(timeout 15m ./bin/$1 -c $2 $query --maxNumberOfMappings 30)
+	results=$(timeout ${timeoutMin}m ./bin/$1 -c $2 $query ${t} ${e} ${m})
 	echo "$results"
 done
