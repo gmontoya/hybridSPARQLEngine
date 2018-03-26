9daa913184ff62f349c47923589406fde0e90143
Gabriela <gmontoya@cs.aau.dk>
2018-03-26 18:29:45 +0200

    adding new branch to fix missing results

--- eval.sh
+++ eval.sh
@@ -8,6 +8,7 @@ endpoint=$7
 e=""
 t=""
 m=""
+o=""
 
 if [ -n "${endpoint}" ]; then
   e="--endpointUrl ${endpoint}"
@@ -21,10 +22,16 @@ if [ -n "${maxNumMappings}" ]; then
   m="--maxNumberOfMappings ${maxNumMappings}"
 fi
 
+if [ -n "${timeoutMin}" ]; then
+  o="--timeoutInMins ${timeoutMin}"
+fi
+
+timeoutMin=$(($timeoutMin+1))
+
 for query in $3/*.rq
 do
 	echo $query
-	echo "timeout ${timeoutMin}m ./bin/$1 -c $2 $query ${t} ${e} ${m}"
-	results=$(timeout ${timeoutMin}m ./bin/$1 -c $2 $query ${t} ${e} ${m})
+	echo "timeout ${timeoutMin}m ./bin/$1 -c $2 $query ${t} ${e} ${m} ${o}"
+	results=$(timeout ${timeoutMin}m ./bin/$1 -c $2 $query ${t} ${e} ${m} ${o})
 	echo "$results"
 done
