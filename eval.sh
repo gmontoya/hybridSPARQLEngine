e84f748abb61eee714e171f906419085fce0b69c
Gabriela <gmontoya@cs.aau.dk>
2018-03-30 12:01:32 +0200

    minor changes in scripts for the second experiment

--- eval.sh
+++ eval.sh
@@ -3,12 +3,14 @@
 timeoutMin=$4
 maxNumMappings=$5
 threshold=$6
-endpoint=$7
+endpoint=$8
+number=$7
 
 e=""
 t=""
 m=""
 o=""
+n=""
 
 if [ -n "${endpoint}" ]; then
   e="--endpointUrl ${endpoint}"
@@ -26,12 +28,16 @@ if [ -n "${timeoutMin}" ]; then
   o="--timeoutInMins ${timeoutMin}"
 fi
 
+if [ -n "${number}" ]; then
+  n="--outputFileNumber ${number}"
+fi
+
 timeoutMin=$(($timeoutMin+1))
 
 for query in $3/*.rq
 do
 	echo $query
-	echo "timeout ${timeoutMin}m ./bin/$1 -c $2 $query ${t} ${e} ${m} ${o}"
-	results=$(timeout ${timeoutMin}m ./bin/$1 -c $2 $query ${t} ${e} ${m} ${o})
+	echo "timeout ${timeoutMin}m ./bin/$1 -c $2 $query ${t} ${e} ${m} ${o} ${n}"
+	results=$(timeout ${timeoutMin}m ./bin/$1 -c $2 $query ${t} ${e} ${m} ${o} ${n})
 	echo "$results"
 done
