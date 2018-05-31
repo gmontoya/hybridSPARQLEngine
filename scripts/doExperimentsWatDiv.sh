a34c38e833510187baaf8d15268709f2622d1083
Gabriela <gmontoya@cs.aau.dk>
2018-05-31 12:42:16 +0200

    improving the implementation of the endpoint iterator

--- scripts/doExperimentsWatDiv.sh
+++ scripts/doExperimentsWatDiv.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-ts="50"
+ts="25 50 75"
+# initial vm index
 ii=0
-nvms=2
+# number of vms
+nvms=4
 # number of clients per vm
-n=2
+n=4
 # total number of clients in the setup
 nc=$((${nvms}*${n}))
 c=true
 resultsFolder=/home/roott/tmp/resultsHybrid_WatDiv_${nc}c_${c}
 # maximum number of queries to execute, -1 to execute all of them
-m=5
+m=-1
 # timeout in minutes
 o=5
 
@@ -18,7 +20,7 @@ techniques="hybridTPF"
 
 for t in ${ts}; do
     mkdir ${resultsFolder}_${t}
-    ./runWatDivExperiments.sh ${ii} ${nvms} ${techniques} ${t} ${n} ${resultsFolder}_${t} ${c} ${m} ${o} > outputRunWatDivExperiments_hybrid_${nc}c_${t}_
+    ./runWatDivExperiments.sh ${ii} ${nvms} ${techniques} ${t} ${n} ${resultsFolder}_${t} ${c} ${m} ${o} > outputRunWatDivExperiments_hybrid_${nc}c_${t}_${c}
     #./deleteOutputs.sh
 done
 
